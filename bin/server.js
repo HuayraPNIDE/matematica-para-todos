3430c46d9466cdb453143a575322e24193f179be
Alumno <alumno@huayra-primaria>
2015-06-15 16:30:17 -0300

    Refactoriza el cliente

--- bin/server.js
+++ bin/server.js
@@ -1,234 +1,257 @@
 var app = require('express')();
+var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var local_ip = require('my-local-ip')()
 var spawn = require('child_process').spawn;
 
-function conectado(socket)
-{
-	var jugador_ip = socket.handshake.address;
-	console.log("Se unio un jugador en "+jugador_ip);
-}
-
-function desconectado(socket)
-{
-	var jugador_ip = socket.handshake.address;
-	console.log("Se fue un jugador en "+jugador_ip+"\n");
-}
-
-function iniciar_servidor(puerto)
-{
-	var jugadores = [];
-	var MAX_JUGADORES = 2;
-
-	var mazo_jugador1 = [];
-	var mazo_jugador2 = [];
-
-	var respuestas = [];
-	var i = 0;
-	var max_respuestas_diferentes = 0;
-
-	var historico_respuestas = [];
-	var opcion_respuestas = [];
-	var obj_ip_jugador = {};
-	var obj_cartas_jugador = {};
-	obj_cartas_jugador['empate'] = 0;
-	obj_cartas_jugador['eliminadas'] = 0;
-	
-	var cartas_guerra = 0;
-
-	io.on('connection', function(socket) {
-
-		socket.on('respuesta', function(opcion) {
-			respuestas.push(jugador_ip);
-			
-			opcion_respuestas.push(opcion);
-
-			//console.log('Tengo '+respuestas.length+' respuestas');
-			console.log(jugador_ip+' = '+opcion);
-			if(respuestas.length == 2) {
-				console.log('Tengo ambas respuestas');
-
-				var lados_j1 = 0;
-				var lados_j2 = 0;
-
-				var img_j1 = 0;
-				var img_j2 = 0;
-
-				if(mazo_jugador1[i].lados > mazo_jugador2[i].lados) {
-					respuesta_real = "jugador1";
-				} else if(mazo_jugador1[i].lados < mazo_jugador2[i].lados) {
-					respuesta_real = "jugador2";
-				} else {
-					respuesta_real = "empate";
-				}
-
-				lados_j1 = mazo_jugador1[i].lados;
-				lados_j2 = mazo_jugador2[i].lados;
-
-				img_j1 = mazo_jugador1[i].img;
-				img_j2 = mazo_jugador2[i].img;
-
-				console.log("Opcion real:" + respuesta_real);
-				console.log("Opciones jugadores: " + opcion_respuestas);
-
-				if(opcion_respuestas[0] == opcion_respuestas[1]) { //Jugador1 y Jugador2 opinan lo mismo
-					if(opcion_respuestas[0] == 'empate') {
-						console.log('Aca hay guerra');
-						cartas_guerra++;
-						obj_cartas_jugador['empate']++;
-					} else {
-						console.log('Las respuestas son iguales, la cartas es para '+opcion_respuestas[0]);
-
-						if(cartas_guerra) {
-							console.log('Hay '+cartas_guerra+' retenidas por guerra\nSe las queda '+opcion_respuestas[0]);
-							obj_cartas_jugador[opcion_respuestas[0]] += cartas_guerra;
-							cartas_guerra = 0;
-						}
-						obj_cartas_jugador[opcion_respuestas[0]]++;
-					}
-
-					historico_respuestas.push({"respuesta_real": respuesta_real, "respuesta_jugadores": opcion_respuestas[0], "lados_jugador1": lados_j1, "lados_jugador2": lados_j2, "img_j1": img_j1, "img_j2": img_j2});
-				} else {
-					console.log('Las respuestas difieren\nRepregunto');
-					i--; //Decremento para que vuelva a enviar la misma mano al emitir
-				}
-
-				if(i == 23) {
-					console.log('Ya se repartieron todas las cartas');
-					console.log('Se muestra historico de respuestas');
-					console.log(historico_respuestas);
-
-					console.log('El jugador1 uno tiene '+obj_cartas_jugador.jugador1+' cartas');
-					console.log('El jugador2 uno tiene '+obj_cartas_jugador.jugador2+' cartas');
-					console.log('Hubo '+obj_cartas_jugador.empate+' empatadas');
-					io.emit('fin', historico_respuestas);
-					i = 0;
-					return;
-				}
-
-				console.log("=======================================================================");
-				i++;
-				console.log('Se juega ahora la mano '+i);
-				io.emit('mano', {"carta1": mazo_jugador1[i], "carta2": mazo_jugador2[i], "contador_jugador1": obj_cartas_jugador.jugador1, "contador_jugador2": obj_cartas_jugador.jugador2, "contador_guerra": cartas_guerra});
-				respuestas = [];
-				opcion_respuestas = [];
-			}
-		});
-
-
-		if(jugadores.length >= MAX_JUGADORES) {
-			console.log('Se conecto el maximo de jugadores: '+jugadores);
-			return;
-		}
-
-
-		var jugador_ip = socket.handshake.address;
-		var nombre_jugador = socket.handshake.query.nombre_jugador;
-
-		if(nombre_jugador == 'jugador1') { //Hack
-			nombre_jugador = usuario;
-		}
-
-		jugadores.push(jugador_ip);
-
-		obj_cartas_jugador[nombre_jugador] = 0;
-	
-
-		obj_ip_jugador[nombre_jugador] = jugador_ip;
-
-		//jugadores++;
-		console.log('Hay conectados '+jugadores.length+' jugadores');
-		conectado(socket);
-
-		if(jugadores.length == MAX_JUGADORES) {
-			console.log("\nYa tenemos a todos los jugadores");
-			console.log("Arrancamos el juego");
-
-
-			console.log(JSON.stringify(obj_cartas_jugador, null, 2));
-
-			var mazo = require('../src/mazo.json');
-			var mazo_completo = mazo.mazo;
-			mazo_completo = mazo_completo.concat(mazo.mazo);
-
-
-			for(var k = 0; k < 24; k++) {
-				var id_carta_jugador1 = Math.floor(Math.random() * mazo_completo.length);
-				var carta_jugador1 = mazo_completo[id_carta_jugador1];
-				mazo_jugador1.push(carta_jugador1);
-				mazo_completo.splice(id_carta_jugador1, 1);
-
-				var id_carta_jugador2 = Math.floor(Math.random() * mazo_completo.length);
-				var carta_jugador2 = mazo_completo[id_carta_jugador2];
-				mazo_jugador2.push(carta_jugador2);
-				mazo_completo.splice(id_carta_jugador2, 1);
-			}
-
-			io.emit('listo', obj_ip_jugador); //Evento para armar interfaz de los clientes
-			console.log('Se juega ahora la mano '+i);
-			io.emit('mano', {"carta1": mazo_jugador1[i], "carta2": mazo_jugador2[i], "contador_jugador1": 0, "contador_jugador2": 0, "contador_guerra": 0});
-		}
-
-		socket.on('disconnect', function(){
-			var index = jugadores.indexOf(jugador_ip);
-			jugadores.splice(index, 1);
-			io.emit('retiro', jugador_ip);
-			desconectado(socket);
-			console.log('Hay conectados '+jugadores.length+' jugadores');
-  		});
-
-	});
-
-
-	http.listen(puerto, function(){
-		console.log('Esperando jugadores en '+puerto);
-	});
-
-}
-
-function publicar_servidor()
-{
-	cliente = spawn('avahi-publish-service',
-			[
-			'-s',
-			'huayra_mxt-'+local_ip+'-'+usuario,
-			'_http._tcp', puerto
-			]
-	);
-
-	cliente.stdout.on('data', function(data) {
-		//console.log("stderr", data);
-	});
-
-	cliente.on('error', function(codigo) {
-		console.error("ERROR: no se puede ejecutar avahi-publish-service", codigo);
-	});
-
-	cliente.on('exit', function(codigo) {
-		if (codigo)
-			console.log("Error, el comando retorno: " + codigo);
-		else
-			console.log("ha finalizado el comando avahi-publish-service");
-	});
-
-}
-
-var usuario;
-
-process.argv.forEach(function (val, index, array) {
-
-	if(/--usuario=/.test(val)) {
-		usuario = val.split('=')[1];
-	}
-});
-
-if(!usuario) {
-	console.log("ERROR: No se especifico usuario");
-	console.log("# node bin/server.js --usuario=Nombre");
-	process.exit(-1);
-}
 
 var puerto = 3000;
+var MAX_JUGADORES = 2;
+var MAX_RESPUESTAS_DIFIRENTES = 0;
+var usuario;
+var jugadores = [];
+
+function conectado(socket) {
+    var jugador_ip = socket.handshake.address;
+    console.log("Se unio un jugador en " + jugador_ip);
+    logger.write("Se unio un jugador en " + jugador_ip);
+}
+
+function desconectado(socket) {
+    var jugador_ip = socket.handshake.address;
+    console.log("Se fue un jugador en " + jugador_ip + "\n");
+    logger.write("Se fue un jugador en " + jugador_ip);
+}
+
+function iniciar_servidor(puerto) {
+    
+    var mazo_jugador1 = [];
+    var mazo_jugador2 = [];
+
+    var respuestas = [];
+    var i = 0;
+
+    var historico_respuestas = [];
+    var opcion_respuestas = [];
+    var obj_ip_jugador = {};
+    var obj_cartas_jugador = {};
+    obj_cartas_jugador['empate'] = 0;
+    obj_cartas_jugador['eliminadas'] = 0;
+    var cartas_guerra = 0;
+
+    io.on('connection', function (socket) {
+        socket.on('respuesta', function (opcion) {
+            respuestas.push(jugador_ip);
+
+            opcion_respuestas.push(opcion);
+
+            //console.log('Tengo '+respuestas.length+' respuestas');
+            console.log(jugador_ip + ' = ' + opcion);
+            logger.write(jugador_ip + ' = ' + opcion);
+            if (respuestas.length == 2) {
+                console.log('Tengo ambas respuestas');
+                logger.write('Tengo ambas respuestas');
+
+                var lados_j1 = 0;
+                var lados_j2 = 0;
+
+                var img_j1 = 0;
+                var img_j2 = 0;
+
+                if (mazo_jugador1[i].lados > mazo_jugador2[i].lados) {
+                    respuesta_real = "jugador1";
+                } else if (mazo_jugador1[i].lados < mazo_jugador2[i].lados) {
+                    respuesta_real = "jugador2";
+                } else {
+                    respuesta_real = "empate";
+                }
+
+                lados_j1 = mazo_jugador1[i].lados;
+                lados_j2 = mazo_jugador2[i].lados;
+
+                img_j1 = mazo_jugador1[i].img;
+                img_j2 = mazo_jugador2[i].img;
+
+                console.log("Opcion real:" + respuesta_real);
+                console.log("Opciones jugadores: " + opcion_respuestas);
+                logger.write("Opcion real:" + respuesta_real);
+                logger.write("Opciones jugadores: " + opcion_respuestas);
+
+                // Jugador1 y Jugador2 opinan lo mismo //
+                if (opcion_respuestas[0] == opcion_respuestas[1]) {
+                    if (opcion_respuestas[0] == 'empate') {
+                        console.log('Aca hay guerra');
+                        logger.write('Aca hay guerra');
+                        cartas_guerra++;
+                        obj_cartas_jugador['empate']++;
+                    } else {
+                        console.log('Las respuestas son iguales, la cartas es para ' + opcion_respuestas[0]);
+                        logger.write('Las respuestas son iguales, la cartas es para ' + opcion_respuestas[0]);
+
+                        if (cartas_guerra) {
+                            console.log('Hay ' + cartas_guerra + ' retenidas por guerra\nSe las queda ' + opcion_respuestas[0]);
+                            logger.write('Hay ' + cartas_guerra + ' retenidas por guerra\nSe las queda ' + opcion_respuestas[0]);
+                            obj_cartas_jugador[opcion_respuestas[0]] += cartas_guerra;
+                            cartas_guerra = 0;
+                        }
+                        obj_cartas_jugador[opcion_respuestas[0]]++;
+                    }
+
+                    historico_respuestas.push({"respuesta_real": respuesta_real, "respuesta_jugadores": opcion_respuestas[0], "lados_jugador1": lados_j1, "lados_jugador2": lados_j2, "img_j1": img_j1, "img_j2": img_j2});
+                } else {
+                    console.log('Las respuestas difieren\nRepregunto');
+                    logger.write('Las respuestas difieren\nRepregunto');
+                    i--; //Decremento para que vuelva a enviar la misma mano al emitir
+                }
+
+                if (i == 23) {
+                    console.log('Ya se repartieron todas las cartas');
+                    console.log('Se muestra historico de respuestas');
+                    console.log(historico_respuestas);
+                    logger.write('Ya se repartieron todas las cartas');
+                    logger.write('Se muestra historico de respuestas');
+                    logger.write(historico_respuestas);
+
+                    console.log('El jugador1 uno tiene ' + obj_cartas_jugador.jugador1 + ' cartas');
+                    console.log('El jugador2 uno tiene ' + obj_cartas_jugador.jugador2 + ' cartas');
+                    console.log('Hubo ' + obj_cartas_jugador.empate + ' empatadas');
+                    logger.write('El jugador1 uno tiene ' + obj_cartas_jugador.jugador1 + ' cartas');
+                    logger.write('El jugador2 uno tiene ' + obj_cartas_jugador.jugador2 + ' cartas');
+                    logger.write('Hubo ' + obj_cartas_jugador.empate + ' empatadas');
+                    io.emit('fin', historico_respuestas);
+                    i = 0;
+                    return;
+                }
+
+                console.log("=======================================================================");
+                logger.write("=======================================================================");
+                i++;
+                console.log('Se juega ahora la mano ' + i);
+                logger.write('Se juega ahora la mano ' + i);
+                io.emit('mano', {"carta1": mazo_jugador1[i], "carta2": mazo_jugador2[i], "contador_jugador1": obj_cartas_jugador.jugador1, "contador_jugador2": obj_cartas_jugador.jugador2, "contador_guerra": cartas_guerra});
+                respuestas = [];
+                opcion_respuestas = [];
+            }
+        });
+
+        if (jugadores.length >= MAX_JUGADORES) {
+            console.log('Se conecto el maximo de jugadores: ' + jugadores);
+            logger.write('Se conecto el maximo de jugadores: ' + jugadores);
+            return;
+        }
+
+        var jugador_ip = socket.handshake.address;
+        var nombre_jugador = socket.handshake.query.nombre_jugador;
+
+        if (nombre_jugador == 'jugador1') { //Hack
+            nombre_jugador = usuario;
+        }
+
+        jugadores.push(jugador_ip);
+
+        obj_cartas_jugador[nombre_jugador] = 0;
+
+        obj_ip_jugador[nombre_jugador] = jugador_ip;
+
+        //jugadores++;
+        console.log('Hay conectados ' + jugadores.length + ' jugadores');
+        logger.write('Hay conectados ' + jugadores.length + ' jugadores');
+        conectado(socket);
+
+        if (jugadores.length == MAX_JUGADORES) {
+            console.log("\nYa tenemos a todos los jugadores");
+            console.log("Arrancamos el juego");
+            console.log(JSON.stringify(obj_cartas_jugador, null, 2));
+            logger.write("\nYa tenemos a todos los jugadores");
+            logger.write("Arrancamos el juego");
+            logger.write(JSON.stringify(obj_cartas_jugador, null, 2));
+
+            var mazo = require('../src/mazo.json');
+            var mazo_completo = mazo.mazo;
+            mazo_completo = mazo_completo.concat(mazo.mazo);
+
+            for (var k = 0; k < 24; k++) {
+                var id_carta_jugador1 = Math.floor(Math.random() * mazo_completo.length);
+                var carta_jugador1 = mazo_completo[id_carta_jugador1];
+                mazo_jugador1.push(carta_jugador1);
+                mazo_completo.splice(id_carta_jugador1, 1);
+
+                var id_carta_jugador2 = Math.floor(Math.random() * mazo_completo.length);
+                var carta_jugador2 = mazo_completo[id_carta_jugador2];
+                mazo_jugador2.push(carta_jugador2);
+                mazo_completo.splice(id_carta_jugador2, 1);
+            }
+
+            io.emit('listo', obj_ip_jugador); //Evento para armar interfaz de los clientes
+            console.log('Se juega ahora la mano ' + i);
+            logger.write('Se juega ahora la mano ' + i);
+            io.emit('mano', {"carta1": mazo_jugador1[i], "carta2": mazo_jugador2[i], "contador_jugador1": 0, "contador_jugador2": 0, "contador_guerra": 0});
+        }
+
+        socket.on('disconnect', function () {
+            var index = jugadores.indexOf(jugador_ip);
+            jugadores.splice(index, 1);
+            io.emit('retiro', jugador_ip);
+            desconectado(socket);
+            console.log('Hay conectados ' + jugadores.length + ' jugadores');
+            logger.write('Hay conectados ' + jugadores.length + ' jugadores');
+        });
+    });
+
+    http.listen(puerto, function () {
+        console.log('Esperando jugadores en ' + puerto);
+        logger.write('Esperando jugadores en ' + puerto);
+    });
+}
+
+function publicar_servidor() {
+    cliente = spawn('avahi-publish-service', [ '-s', 'huayra_mxt-' + local_ip + '-' + usuario, '_http._tcp', puerto]);
+
+    cliente.stdout.on('data', function (data) {
+        //console.log("stderr", data);
+    });
+
+    cliente.on('error', function (codigo) {
+        console.error("ERROR: no se puede ejecutar avahi-publish-service", codigo);
+        logger.write("ERROR: no se puede ejecutar avahi-publish-service", codigo);
+    });
+
+    cliente.on('exit', function (codigo) {
+        if (codigo) {
+            console.log("Error, el comando retorno: " + codigo);
+            logger.write("Error, el comando retorno: " + codigo);
+        } else {
+            console.log("ha finalizado el comando avahi-publish-service");
+            logger.write("ha finalizado el comando avahi-publish-service");
+        }
+    });
+}
+
+process.argv.forEach(function (val, index, array) {
+    if (/--usuario=/.test(val)) {
+        usuario = val.split('=')[1];
+    }
+});
+
+if (!usuario) {
+    console.log("ERROR: No se especifico usuario");
+    console.log("# node bin/server.js --usuario=Nombre");
+    logger.write("ERROR: No se especifico usuario");
+    logger.write("# node bin/server.js --usuario=Nombre");
+    process.exit(-1);
+}
+
 iniciar_servidor(puerto);
 publicar_servidor();
+
+var LoggerFile = function() {
+    this.filename = '/tmp/log.txt';
+    fs.writeFileSync(this.filename, "INICIO\n");
+    this.write = function (data) {
+        fs.appendFileSync(this.filename, data + "\n");
+    }
+    fs.appendFileSync(this.filename, "==========================================\n");
+}
+
+var logger = new LoggerFile();
---
+++ src/js/main.js
@@ -0,0 +1,131 @@
+var puerto = 3000;
+
+var Servidor = function(nombre) {
+    spawn = require('child_process').spawn;
+    spawn('nodejs', ['../bin/server.js', "--usuario="+nombre]);
+};
+
+var Conexion = function (ip, nombreJugador) {
+    this.socket = io("http://" + ip + ":" + puerto + "/", {query: "nombre_jugador=" + nombreJugador});
+    this.registrarEspera = function() {
+        var self = this;
+        self.socket.on('connect', function () {
+            console.log('Me conecte al servidor');
+
+            self.socket.on('listo', function (o) {
+                $("#mazos").show();
+                $("#lista_amigos, #btn_actualizar_amigos").hide();
+                $("#titulo").html('Arranca el juego');
+            });
+
+            self.socket.on('retiro', function (msg) {
+                $("#titulo").html(msg + ' Se retiro <br> Es el fin del juego');
+                $("#mazos, #opciones, #cartas").hide();
+                $("#nombres").html('');
+                self.socket.disconnect();
+            });
+
+            self.socket.on('fin', function (resultados) {
+                $("#titulo").html('Es el fin del juego <br> Estos son los resultados');
+                $("#mazos, #opciones, #cartas").hide();
+                self.socket.disconnect();
+            });
+
+            self.socket.on('mano', function (o) {
+                console.log('Desde el server me llegan cartas:');
+                console.log(JSON.stringify(o, null, 2));
+                $("#cont_jugador1").html(o.contador_jugador1);
+                $("#cont_jugador2").html(o.contador_jugador2);
+                $("#cont_guerra").html(o.contador_guerra);
+                $("#mazos, #opciones").show();
+                // Cambia las cartas para la nueva mano
+                console.log($("#carta_jugador1").find("img"));
+                $("#carta_jugador1").find("img").prop("src", o.carta1.img).on('click', function () {
+                    this.socket.emit('respuesta', usuario_info.nombre);
+                });
+                console.log($("#carta_jugador1").find("img").prop("src"));
+                $("#carta_jugador2").find("img").prop("src", o.carta2.img);
+                $("#mazos, #opciones, #cartas").show();
+
+            });
+
+//                btn_mano.onclick = function () {
+//                    console.log('Respondo');
+//                    var eleccion = document.getElementsByName("opcion");
+//                    var val = "";
+//                    for (var j = 0; j < eleccion.length; j++) {
+//                        if (eleccion[j].checked) {
+//                            val = eleccion[j].value;
+//                        }
+//                    }
+//                    this.socket.emit('respuesta', val);
+//                    btn_mano.disabled = true;
+//                };
+
+            });
+        }
+}
+
+var Cliente = function(nombre, avatar) {
+    this.nombre = nombre;
+    this.avatar = avatar;
+    this.localIp = require('my-local-ip')();
+    this.init = function() {
+        var conexion = new Conexion(this.localIp, this.nombre);
+        $("#avatar").hide();
+        $("#btn_actualizar_amigos, #lista_amigos").show();
+        conexion.registrarEspera();
+    }
+};
+
+var Amigos = function () {
+    this.amigos = {};
+    this.buscar = function() {
+        this.spawn = require('child_process').spawn;
+        this.proceso = this.spawn("avahi-browse", ['-a', '-r', '-p', '--ignore-local']);
+        this.lineas;
+        var self = this;
+
+        this.proceso.stdout.on("data", function (chunk) {
+            this.lineas = ("" + chunk).split("\n");
+            for (i = 0; this.lineas.length > i; i++) {
+                if (/huayra_mxt/.test(this.lineas[i])) {
+                    campos = this.lineas[i].split(";");
+
+                    // tipo
+                    if (campos[0] == '=') {
+                        ip = campos[3].split('-')[1].replace(/\\/gi, "");
+                        nombre = campos[3].split('-')[2];
+                        self.amigos[ip] = nombre;
+                    }
+
+                    if (campos[0] == '-') {
+                        ip = campos[3].split('-')[1].replace(/\\/gi, "");
+                        delete self.amigos[ip];
+                    }
+                }
+            }
+        });
+    },
+    this.actualizarVista = function() {
+        var self = this;
+        e = $("<div></div>");
+        for (var k in this.amigos) {
+            amigo = this.amigos[k];
+            tmp = '<div class="media" data-ip="' + k + '" data-nombre="' + this.amigos[k] + '"> \
+              <div class="media-left"><a href="#"><img class="media-object" src="res/gui/avatar-neutro.png"></a></div> \
+              <div class="media-body"><h4 class="media-heading">' + this.amigos[k] + '</h4></div> \
+            </div>';
+
+            $(e).append($(tmp).on('click', function() {
+                self.elegir($(this).data('ip'), $(this).data('nombre'));
+            }));
+        }
+        $('#lista_amigos').html(e);
+        $('#lista_amigos').show();
+    },
+    this.elegir = function(ip, nombreJugador) { // Se conecta al Servidor elegido //
+        var conexion = new Conexion(ip, nombreJugador);
+        conexion.registrarEspera();
+    }
+};
