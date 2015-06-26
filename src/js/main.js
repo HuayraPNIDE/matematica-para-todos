4a26c300b730e590a06c72bcc144af2f7da060b7
Alumno <alumno@huayra-primaria>
2015-06-26 12:42:35 -0300

    Corrige SOCKET en el cliente.

--- src/js/main.js
+++ src/js/main.js
@@ -9,21 +9,18 @@ var Servidor = function(nombre) {
 };
 
 var Conexion = function (ip, nroJugador, nombreJugador) {
-    socket = io("http://" + ip + ":" + PUERTO + "/", {query: 'nro_jugador=' + nroJugador + "&nombre_jugador=" + nombreJugador});
-    
-//    var self = this;
-//    this.registrarEspera = function() {
-        socket.on('connect', function () {
-            
-//            console.log('Me conecte al servidor');
-            socket.on('listo', function (o) {
-                $("#amigos").hide();
-                $("#titulo").html('A Jugar!!!');
-                $(".jugador1 .avatar").html(o.jugador1.nombre + "(" + o.jugador1.ip + ")");
-                $(".jugador2 .avatar").html(o.jugador2.nombre + "(" + o.jugador2.ip + ")");
-                $(".juego").show();
-            });
-//
+    var socket = io("http://" + ip + ":" + PUERTO + "/", {query: 'nro_jugador=' + nroJugador + "&nombre_jugador=" + nombreJugador});
+    console.log('Conexion');
+
+    socket.on('connect', function () {
+        socket.on('listo', function (o) {
+            $("#amigos").hide();
+            $("#titulo").html('A Jugar!!!');
+            $(".jugador1 .avatar").html(o.jugador1.nombre + "(" + o.jugador1.ip + ")");
+            $(".jugador2 .avatar").html(o.jugador2.nombre + "(" + o.jugador2.ip + ")");
+            $(".juego").show();
+        });
+
 //            socket.on('retiro', function (msg) {
 //                $("#titulo").html(msg + ' Se retiro <br> Es el fin del juego');
 //                $(".juego").hide();
@@ -36,50 +33,45 @@ var Conexion = function (ip, nroJugador, nombreJugador) {
 //                socket.disconnect();
 //            });
 //
-            socket.on('mano', function (o) {
-                console.log("mano");
-                setInterval(function(){
-                    socket.emit('respuesta', {llave: nroJugador});
-//                    console.log("Cada 2.5 segundos" + JSON.stringify(socket, null, 2));
-                    console.log("Cada 2.5 segundos");
-                    console.dir(socket);
-                }, 2500);
+        socket.on('mano', function (o) {
+//            setInterval(function(){
+//                socket.emit('respuesta', {llave: nroJugador});
+////                    console.log("Cada 2.5 segundos" + JSON.stringify(socket, null, 2));
+//                console.log("Cada 2.5 segundos");
+//                console.dir(socket);
+//            }, 2500);
+//                
+            // Contador
+            if(o.jugador1.contador || $(".jugador1 .contador img").prop('src') !=  IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION) {
+                $(".jugador1 .contador img").prop('src', IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION);
+            }
+            $(".jugador1 .contador figcaption").html(o.jugador1.contador);
 
-//                console.log('Desde el server me llegan cartas:');
-//                console.log(JSON.stringify(o, null, 2));
-//                
-                // Contador
-                if(o.jugador1.contador || $(".jugador1 .contador img").prop('src') !=  IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION) {
-                    $(".jugador1 .contador img").prop('src', IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION);
-                }
-                $(".jugador1 .contador figcaption").html(o.jugador1.contador);
-                
-                if(o.jugador2.contador || $(".jugador2 .contador img").prop('src') !=  IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION) {
-                    $(".jugador2 .contador img").prop('src', IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION);
-                }
-                $(".jugador2 .contador figcaption").html(o.jugador2.contador);
-//                
-//                // Cartas
-//                $("#mano .jugador1").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador1.carta.img + IMG_EXTENSION);
-//                $("#mano .jugador2").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador2.carta.img + IMG_EXTENSION);
-//                // Cuando hay Guerra
-//                if(o.contador_guerra) {
-//                    $("#mano .guerra .contador figcaption").html(o.contador_guerra);
-//                }
-//                
-//                $("#mano .respuesta").on('click', function() {
-//                    // Deshabilita cartas //
-//                    $("#mano .jugador1").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador1.carta.img + '_deshabilitado' + IMG_EXTENSION);
-//                    $("#mano .jugador2").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador2.carta.img + '_deshabilitado' + IMG_EXTENSION);
-//                    // Envia la selección //
-//                    socket.emit('respuesta', { jugador: nroJugador, respuesta: $(this).find('img').prop('class') });
-//                    $("#mano .respuesta").off('click');
-//                });
-                
-                $("#mano").show();
+            if(o.jugador2.contador || $(".jugador2 .contador img").prop('src') !=  IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION) {
+                $(".jugador2 .contador img").prop('src', IMG_CARPETA + 'caja_cartas' + IMG_EXTENSION);
+            }
+            $(".jugador2 .contador figcaption").html(o.jugador2.contador);
+
+            // Cartas
+            $("#mano .jugador1").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador1.carta.img + IMG_EXTENSION);
+            $("#mano .jugador2").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador2.carta.img + IMG_EXTENSION);
+            // Cuando hay Guerra
+            if(o.contador_guerra) {
+                $("#mano .guerra .contador figcaption").html(o.contador_guerra);
+            }
+
+            $("#mano .respuesta").on('click', function() {
+                // Deshabilita cartas //
+                $("#mano .jugador1").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador1.carta.img + '_deshabilitado' + IMG_EXTENSION);
+                $("#mano .jugador2").prop("src", IMG_CARPETA + IMG_NOMBRE + o.jugador2.carta.img + '_deshabilitado' + IMG_EXTENSION);
+                // Envia la selección //
+                socket.emit('respuesta', { jugador: nroJugador, respuesta: $(this).find('img').prop('class') });
+                $("#mano .respuesta").off('click');
             });
+
+            $("#mano").show();
         });
-//    };
+    });
 };
 
 var Cliente = function(nombre, avatar) {
